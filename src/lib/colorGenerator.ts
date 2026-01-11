7fcc4261541558617b71afe467eb11b48989d26b
Malo Landemaine <malo.landemaine@gmail.com>
2026-01-11 21:32:26 +0100

    feat(wheel): no more too light shade

--- src/lib/colorGenerator.ts
+++ src/lib/colorGenerator.ts
@@ -1,5 +1,5 @@
 export function colorGenerator() {
-  const greyShade = Math.floor(Math.random() * 256);
+  const greyShade = Math.floor(Math.random() * 156) + 50;
   const hexShade = greyShade.toString(16).padStart(2, "0");
   return `#${hexShade}${hexShade}${hexShade}`;
 }
